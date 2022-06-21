a2eccb179de765ebd368b18c11b70d17ddf0d485
tDohrand <107406758+tDohrand@users.noreply.github.com>
2022-06-21 13:01:27 +0200

    Add files via upload

--- pyproject.py
+++ pyproject.py
@@ -3,12 +3,7 @@ a = 0
 b = 0
 c = 0
 d = 0
-print("Welcome to the Employee Management Program (EMP)")
-print("==================================================")
-print("Please choose one of the following options:")
-import os
-while (z != 6):
-    print("""
+mainmenu = ("""
     1.create
     2.read
     3.update
@@ -16,33 +11,67 @@ while (z != 6):
     5.save / export
     6.end program
     """)
-    z = int(input("Please enter a number between 1 and 6:"))
-    if z == 1:
-        print("==================================================")
-        print("CREATE")
-        print("==================================================")
-        while (a !=4):
-            print("""
-            1.Create new database
-            2.Create new entry
-            3.add new column (for all datasets)
+menu1 = ("""
+            1.Create new folder
+            2.Create new file
+            3.Show file tree
             4.back
             """)
+menu2 = ("""
+            1. Read file
+            2. Write file
+            3. 
+            4. show empty fields
+            5. back
+            """)
+menu3 = ("""
+            1. update all
+            2. Change file name and type.
+            3. Show directory tree.
+            4. Back
+            """)
+menu4 = ("""
+            1. delete all
+            2. delete single row
+            3. delete single column
+            4. back
+            """)
+
+print("Welcome to the Employee Management Program (EMP)")
+print("==================================================")
+print("Please choose one of the following options:")
+import os
+while (z != 6):
+    print(mainmenu)
+    z = int(input("Please enter a number between 1 and 6:"))
+    if z == 1:
+        while (a !=4):
+            print("==================================================")
+            print("CREATE")
+            print("==================================================")
+            print(menu1)
             a = int(input("Please enter a number between 1 and 4:"))
             if a == 1:
                 foldername = input("Enter new folder name:")
                 folderpath = input("Enter path for new folder:")
-                import.os
-                os.mkdir(C:/pyproject/newfolder/)
-                a = int(input("Please enter a number between 1 and 4:"))
+                import os
+                os.mkdir(folderpath + foldername)
+                print(foldername + " successfully created at " + folderpath)
             elif a == 2:
-                filename1 = print(input("Enter file name:"))
-                filetype1 = print(input("Enter file type:"))
-                open(str(filename1) + "." + str(filetype1), "x")
-                a = int(input("Please enter a number between 1 and 4:"))
+                filename1 = input("Enter file name:")
+                filetype1 = input("Enter file type:")
+                filepath1 = input("Enter file path:")
+                open(filepath1 + filename1 + filetype1, "x")
+                print(filename1 + " successfully created at " + filepath1)
             elif a == 3:
-                print("3.add new column (for all datasets)")
-                a = int(input("Please enter a number between 1 and 4:"))
+                print("File tree for C:/pyproject/")
+                def tree_printer(root):
+                    for root, dirs, files in os.walk(root):
+                        for d in dirs:
+                            print(os.path.join(root, d))
+                        for f in files:
+                            print(os.path.join(root, f))
+                tree_printer('C:/pyproject/')
             elif a == 4:
                 print("Back to the menu")
     elif z == 2:
@@ -50,13 +79,7 @@ while (z != 6):
         print("READ")
         print("==================================================")
         while (b !=5):
-            print("""
-            1. Open Database
-            2. show single dataset
-            3. filter ...
-            4. show empty fields
-            5. back
-            """)
+            print(menu2)
             b = int(input("Please enter a number between 1 and 5:"))
             if b == 1:
                 print("1. show all datasets")
@@ -76,32 +99,35 @@ while (z != 6):
         print("==================================================")
         print("UPDATE")
         print("==================================================")
-        while (c !=3):
-            print("""
-            1. update all
-            2. update single dataset
-            3. back
-            """)
+        while (c !=4):
+            print(menu3)
             c = int(input("Please enter a number between 1 and 3:"))
             if c == 1:
                 print("1. update all")
                 c = int(input("Please enter a number between 1 and 3:"))
             elif c == 2:
                 print("2. update single dataset")
-                c = int(input("Please enter a number between 1 and 3:"))
+                directorypath = input("Set working directory path:")
+                file1 = input("Old file name:")
+                file2 = input("New file name:")
+                os.chdir(directorypath)
+                os.rename(file1, file2)
+                print("File " + file1 + " was renamed to " + file2)
             elif c == 3:
-                print("Back to the menu")
+                print("File tree for C:/pyproject/")
+                def tree_printer(root):
+                    for root, dirs, files in os.walk(root):
+                        for d in dirs:
+                            print(os.path.join(root, d))
+                        for f in files:
+                            print(os.path.join(root, f))
+                tree_printer('C:/pyproject/')
     elif z == 4:
         print("==================================================")
         print("DELETE")
         print("==================================================")
         while (d !=4):
-            print("""
-            1. delete all
-            2. delete single row
-            3. delete single column
-            4. back
-            """)
+            print(menu4)
             d = int(input("Please enter a number between 1 and 4:"))
             if d == 1:
                 print("1. delete all")
