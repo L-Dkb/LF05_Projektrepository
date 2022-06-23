03085cb63bbb4bca5fc5986becc114a1dc498200
tDohrand <107406758+tDohrand@users.noreply.github.com>
2022-06-23 09:38:51 +0200

    Add files via upload

--- pyproject.py
+++ pyproject.py
@@ -1,8 +1,11 @@
+import os
+import os.path
 z = 0
 a = 0
 b = 0
 c = 0
 d = 0
+#Menus
 mainmenu = ("""
     1.create
     2.read
@@ -19,7 +22,7 @@ menu1 = ("""
             """)
 menu2 = ("""
             1. Read file
-            2. Write file
+            2. Read specific line in file
             3. 
             4. show empty fields
             5. back
@@ -41,7 +44,8 @@ spaceholder1 = ("==================================================")
 print("Welcome to the Employee Management Program (EMP)")
 print(spaceholder1)
 print("Please choose one of the following options:")
-import os
+
+#MAIN MENU
 while (z != 6):
     print(spaceholder1)
     print("MAIN MENU")
@@ -49,6 +53,8 @@ while (z != 6):
     print(mainmenu)
     print(spaceholder1)
     z = int(input("Please enter a number between 1 and 6:"))
+
+#CREATE MENU
     if z == 1:
         while (a !=4):
             print(spaceholder1)
@@ -56,18 +62,20 @@ while (z != 6):
             print(spaceholder1)
             print(menu1)
             a = int(input("Please enter a number between 1 and 4:"))
+#CREATE FOLDER FUNCTION
             if a == 1:
                 foldername = input("Enter new folder name:")
                 folderpath = input("Enter path for new folder:")
-                import os
                 os.mkdir(folderpath + foldername)
                 print(foldername + " successfully created at " + folderpath)
+#CREATE FILE FUNCTION
             elif a == 2:
                 filename1 = input("Enter file name:")
                 filetype1 = input("Enter file type:")
                 filepath1 = input("Enter file path:")
                 open(filepath1 + filename1 + filetype1, "x")
                 print(filename1 + " successfully created at " + filepath1)
+#SHOP DIRECTORY TREE
             elif a == 3:
                 print("File tree for C:/pyproject/")
                 def tree_printer(root):
@@ -77,8 +85,10 @@ while (z != 6):
                         for f in files:
                             print(os.path.join(root, f))
                 tree_printer('C:/pyproject/')
+#BACK TO MAIN MENU
             elif a == 4:
                 print("Back to the menu")
+#READ MENU
     elif z == 2:
         print(spaceholder1)
         print("READ")
@@ -86,15 +96,49 @@ while (z != 6):
         while (b !=5):
             print(menu2)
             b = int(input("Please enter a number between 1 and 5:"))
+#READ FILE FUNCTION
             if b == 1:
-                path = input("Enter file path and name")
-                x = open(path, "r")
-                print("Showing " C:/pyproject/test123.txt " contents...")
-                print(x.read())
+                spaceholder1 = ("==================================================")
+                path = input("Enter file path and name:")
+                path_check = os.path.exists(path)
+                if path_check:
+                    x = open(path, "r")
+                    print("Showing " + path + " contents...")
+                    print(x.read())
+                else:
+                    print(spaceholder1)
+                    print("Requested file does not exist! Check path and try again!")
+                    print(spaceholder1)
+                    def tree_printer(root):
+                        for root, dirs, files in os.walk(root):
+                            for d in dirs:
+                                print(os.path.join(root, d))
+                            for f in files:
+                                print(os.path.join(root, f))
+                    tree_printer('C:/pyproject/')
+                    print(spaceholder1)
+#READ FILE LINE FUNCTION
             elif b == 2:
-                print("2. show single dataset")
-                b = int(input("Please enter a number between 1 and 5:"))
-            elif b == 3:
+                spaceholder1 = ("==================================================")
+                path = input("Enter file path and name:")
+                path_check = os.path.exists(path)
+                if path_check:
+                    line = int(input("Enter line number:"))
+                    x = open(path, "r")
+                    print("Showing " + line + " of file " + path)
+                    print(x.readline(line))
+                else:
+                    print(spaceholder1)
+                    print("Requested file does not exist! Check path and try again!")
+                    print(spaceholder1)
+                    def tree_printer(root):
+                        for root, dirs, files in os.walk(root):
+                            for d in dirs:
+                                print(os.path.join(root, d))
+                            for f in files:
+                                print(os.path.join(root, f))
+                    tree_printer('C:/pyproject/')
+                    print(spaceholder1)
                 print("3. filter ...")
                 b = int(input("Please enter a number between 1 and 5:"))
             elif b == 4:
@@ -102,6 +146,7 @@ while (z != 6):
                 b = int(input("Please enter a number between 1 and 5:"))
             elif b == 5:
                 print("Back to the menu")
+#UPDATE MENU
     elif z == 3:
         print(spaceholder1)
         print("UPDATE")
@@ -112,6 +157,7 @@ while (z != 6):
             if c == 1:
                 print("1. update all")
                 c = int(input("Please enter a number between 1 and 3:"))
+#RENAME FILE FUNCTION
             elif c == 2:
                 print("2. update single dataset")
                 directorypath = input("Set working directory path:")
@@ -129,6 +175,7 @@ while (z != 6):
                         for f in files:
                             print(os.path.join(root, f))
                 tree_printer('C:/pyproject/')
+#DELETE MENU
     elif z == 4:
         print(spaceholder1)
         print("DELETE")
