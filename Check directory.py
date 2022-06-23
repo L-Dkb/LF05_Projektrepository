b484576e4f2ec4fa5c2a3bb199e94ef43b8a3c73
tDohrand <107406758+tDohrand@users.noreply.github.com>
2022-06-23 09:06:59 +0200

    Add files via upload

--- Check directory.py
+++ Check directory.py
@@ -1,9 +1,11 @@
 import os
-path = input("Enter file path:")
+path = input("Enter file path and name")
 path_check = os.path.exists("path")
 if path_check == "true":
 else:
-    print("Requested file does not exist! Check path and try again")
+    print(spaceholder1)
+    print("Requested file does not exist! Check path and try again!")
+    print(spaceholder1)
     def tree_printer(root):
         for root, dirs, files in os.walk(root):
             for d in dirs:
@@ -11,3 +13,4 @@ else:
             for f in files:
                 print(os.path.join(root, f))
     tree_printer('C:/pyproject/')
+    print(spaceholder1)
